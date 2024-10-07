4482125c12796169ec4bca51ac48284a2fd34df4
Rhss <3567878896@qq.com>
2024-10-07 18:39:22 +0800

    Bean生命周期

---
+++ src/main/java/com/example/springbeanlifecycledemo/SpringBeanLifeCycleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbeanlifecycledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanLifeCycleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanLifeCycleDemoApplication.class, args);
+    }
+
+}
